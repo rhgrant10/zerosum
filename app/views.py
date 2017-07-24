e47fc807fdd2b11bbf7066141ef1cf3fcb488938
Robert Grant <rhgrant10@gmail.com>
2017-07-23 21:44:16 -0400

    Add a move search api using Flask

---
+++ app/views.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+from flask import jsonify
+from flask import request
+from flask import abort
+
+from app import app
+from zerosum import solvers
+from zerosum import base
+from zerosum.examples import tictactoe
+
+
+solvers_map = {
+    'minimax': solvers.Minimax,
+    'negamax': solvers.Negamax,
+    'alphabeta': solvers.AlphaBeta,
+}
+
+
+evaluators_map = {
+    'smart': tictactoe.SmartEvaluator,
+    'simple': tictactoe.SimpleEvaluator,
+}
+
+
+@app.route('/')
+def index():
+    api_description = {
+        '/api/search/': {
+            'evaluators': list(evaluators_map),
+            'solvers': list(solvers_map),
+            'maxDepth': list(range(10)),
+        }
+    }
+    return jsonify(api_description)
+
+
+@app.route('/api/search', methods=['POST'])
+def search():
+    data = request.json
+    if not data or 'board' not in data or 'players' not in data:
+        abort(400)
+
+    board = tictactoe.Board(squares=data['board'], players=data['players'])
+
+    Evaluator = evaluators_map[data.get('evaluator', 'smart')]
+    Solver = solvers_map[data.get('solver', 'minimax')]
+    max_depth = data.get('maxDepth', 5)
+
+    solver = Solver(evaluator=Evaluator(), max_depth=max_depth)
+    scored_move = solver.search(board)
+    result = dict(zip(['score', 'move'], scored_move))
+
+    response = {
+        'result': result,
+        'solver': Solver.__name__,
+        'evaluator': Evaluator.__name__,
+        'maxDepth': max_depth,
+    }
+    return jsonify(response)
---
+++ config.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# Enable Flask's debugging features. Should be False in production
+DEBUG = True
